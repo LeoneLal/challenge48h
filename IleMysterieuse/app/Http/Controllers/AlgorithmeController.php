4d2a41be517e3cee9298b7a059aa4e2d4423f24f
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-02-02 01:14:23 +0100

    Update text for enigme 3

--- IleMysterieuse/app/Http/Controllers/AlgorithmeController.php
+++ IleMysterieuse/app/Http/Controllers/AlgorithmeController.php
@@ -14,7 +14,7 @@ class AlgorithmeController extends Controller
         $hours = request()->input('hours');
         $minutes = request()->input('minutes');
         if($hours == 600 && $minutes == 35){
-            $result_exo1 = "Félicitation ! Enigme 3.1 : ! Nouvel indice : IX-38-13-2";
+            $result_exo1 = "Félicitation ! Enigme 3.1 : ! Nouvel indice : IX-38-13-2   Allez voir Jules Vernes";
         }else{
             $result_exo1 = "Essaie encore";
         };
@@ -25,7 +25,7 @@ class AlgorithmeController extends Controller
     public function exo2(Request $request){
         $date = request()->input('date');
         if($date == 1869){
-            $result_exo2 = "Félicitation ! Enigme 3.2 : ! Nouvel indice : XXV-102-9-4";
+            $result_exo2 = "Félicitation ! Enigme 3.2 : A Nouvel indice : XXV-102-9-4    Allez voir Jules Vernes";
         }else{
             $result_exo2 = "Essaie encore";
         };
